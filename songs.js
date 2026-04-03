968dba83cb469907b6b155c864d3cfe2ea63a3f4
yoyou458 <yousefeltahawyashraf@gmail.com>
2026-04-02 19:41:39 -0700

    Update songs.js

--- songs.js
+++ songs.js
@@ -4,7 +4,7 @@ const songs = [
     {name:"ما تقوله ليه", file:"songs/ma_t2olou_leh.mp3", img:"ma_t2olou_leh.jpg"},
     {name:"كلام الناس", file:"songs/kalam_el_nas.mp3", img:"kalam_el_nas.jpg"},
     {name:"خسرت كل الناس", file:"songs/khesert_kol_el_nas.mp3", img:"khasart_kol_elnas.jpg"},
-    {name:"اسمعيلي كلمة", file:"songs/isma3i_kelma.mp3", img:"isma3i_kelma.jpg"}, 
+    {name:"اسمعيلي كلمة", file:"isma3i_kelma.mp3", img:"isma3i_kelma.jpg"}, 
     {name:"يا بختك يالي صاحي الليل", file:"songs/sa7i_el_lail.mp3", img:"sa7i_el_lail.jpg"},
     {name:"طبيب جراح ", file:"songs/tabeb_garah.mp3", img:"tabeb_garah.jpg"} ,
     {name:"يوم الوداع ", file:"songs/youm_el_wada3.mp3", img:"youm_el_wada3.jpg"} ,
